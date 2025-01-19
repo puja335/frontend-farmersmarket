1b4cb59fbdc9b064f7ad6043182f25e68add1cb0
puja335 <shahipuja335@gmail.com>
2025-01-19 07:58:17 +0545

    added layout.jsx

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,17 +1,16 @@
-import { createContext } from "react";
-import { Outlet, useLocation } from "react-router-dom";
-import { useAuth } from "../../hooks/useAuth";
-import Breadcrumb from "../Braedcrumb/Breadcrumb";
-import Footer from "../Footer/Footer";
-import Navbar from "../Navbar/Navbar";
-import { useState } from "react";
+import { createContext, useState } from "react"
+import { Outlet, useLocation } from "react-router-dom"
+import { useAuth } from "../../hooks/useAuth"
+import Breadcrumb from "../Braedcrumb/Breadcrumb"
+import Footer from "../Footer/Footer"
+import Navbar from "../Navbar/Navbar"
 
-export const groceryContext = createContext();
+export const groceryContext = createContext()
 const Layout = () => {
-  const { isAuthenticated } = useAuth();
-  const location = useLocation();
-  const isHomePage = location.pathname === "/";
-  const [cartItems, setCartItems] = useState([]);
+  const { isAuthenticated } = useAuth()
+  const location = useLocation()
+  const isHomePage = location.pathname === "/"
+  const [cartItems, setCartItems] = useState([])
   return (
     <groceryContext.Provider
       value={{
@@ -26,7 +25,7 @@ const Layout = () => {
       </section>
       <Footer />
     </groceryContext.Provider>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
