0ecfa608db16bdfcf7f886ca17c1eaaadbf94f63
puja335 <shahipuja335@gmail.com>
2025-01-22 17:02:23 +0545

    added adminroute.jsx

--- src/Components/ProtectedRoute/AdminRoutes.jsx
+++ src/Components/ProtectedRoute/AdminRoutes.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect } from "react";
-import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom";
-import { useAuth } from "../../hooks/useAuth";
+import React, { useEffect } from "react"
+import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom"
+import { useAuth } from "../../hooks/useAuth"
 
 const AdminRoutes = () => {
-  const location = useLocation();
-  const { isAuthenticated, user } = useAuth();
-  const navigate = useNavigate();
+  const location = useLocation()
+  const { isAuthenticated, user } = useAuth()
+  const navigate = useNavigate()
 
   useEffect(() => {
     if (!isAuthenticated) {
-      navigate("/login");
+      navigate("/login")
     }
-  }, [isAuthenticated]);
+  }, [isAuthenticated])
   return isAuthenticated && user.role == "admin" ? (
     <Outlet />
   ) : (
     <Navigate to={"/login"} state={{ from: location }} />
-  );
-};
+  )
+}
 
-export default AdminRoutes;
+export default AdminRoutes
