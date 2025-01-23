f4e42f1678031de378099130f9aaf26c254880d7
puja335 <shahipuja335@gmail.com>
2025-01-23 17:05:49 +0545

    added useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,51 +1,51 @@
-import { useCallback } from "react";
-import { useNavigate } from "react-router-dom";
-import { logIn, logout, register, updateUser } from "../features/auth/authSlice";
-import useAppSelector from "./useAppSelector";
-import useAppDispatch from "./useDispatch";
+import { useCallback } from "react"
+import { useNavigate } from "react-router-dom"
+import { logIn, logout, register, updateUser } from "../features/auth/authSlice"
+import useAppSelector from "./useAppSelector"
+import useAppDispatch from "./useDispatch"
 
 export const useAuth = () => {
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
   const { user, isAuthenticated, isLoading, error } = useAppSelector(
     (state) => state.auth
-  );
+  )
 
   const login = useCallback(
     async (email, password) => {
       try {
-        await dispatch(logIn({ email, password })).unwrap();
-        return true;
+        await dispatch(logIn({ email, password })).unwrap()
+        return true
       } catch (error) {
-        return false;
+        return false
       }
     },
     [dispatch]
-  );
+  )
 
   const signup = useCallback(
     async (userData) => {
       try {
-        await dispatch(register(userData)).unwrap();
-        return true;
+        await dispatch(register(userData)).unwrap()
+        return true
       } catch (error) {
-        return false;
+        return false
       }
     },
     [dispatch]
-  );
+  )
 
   const signOut = useCallback(() => {
-    dispatch(logout());
-    navigate("/login");
-  }, [dispatch,navigate]);
+    dispatch(logout())
+    navigate("/login")
+  }, [dispatch, navigate])
 
   const updateProfile = useCallback(
     (userData) => {
-      dispatch(updateUser(userData));
+      dispatch(updateUser(userData))
     },
     [dispatch]
-  );
+  )
 
   return {
     user,
@@ -56,7 +56,7 @@ export const useAuth = () => {
     signup,
     logout: signOut,
     updateProfile,
-  };
-};
+  }
+}
 
 // export default useAuth;
