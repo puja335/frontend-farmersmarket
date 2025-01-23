b87b3bf5725f7c17acb5915e01086bfc0542c5a5
puja335 <shahipuja335@gmail.com>
2025-01-23 17:05:10 +0545

    added whishlistSlice.js

--- src/features/wishlist/wishlistSlice.js
+++ src/features/wishlist/wishlistSlice.js
@@ -1,44 +1,44 @@
 // src/store/wishlistSlice.ts
-import { createSlice } from "@reduxjs/toolkit";
-import { toast } from "react-toastify";
+import { createSlice } from "@reduxjs/toolkit"
+import { toast } from "react-toastify"
 
 const initialState = {
   items: [],
-};
+}
 
 const wishlistSlice = createSlice({
   name: "wishlist",
   initialState,
   reducers: {
     addItemToWishlist: (state, action) => {
-      const newItem = action.payload;
-      const existingItem = state.items.find((item) => item.id === newItem.id);
+      const newItem = action.payload
+      const existingItem = state.items.find((item) => item.id === newItem.id)
 
       if (!existingItem) {
-        state.items.push(newItem);
-        toast.success("Item added to wishlist successfully!");
+        state.items.push(newItem)
+        toast.success("Item added to wishlist successfully!")
       } else {
-        toast.info("Item is already in the wishlist.");
+        toast.info("Item is already in the wishlist.")
       }
     },
     removeItemFromWishlist: (state, action) => {
-      const id = action.payload;
-      const existingItem = state.items.find((item) => item.id === id);
+      const id = action.payload
+      const existingItem = state.items.find((item) => item.id === id)
 
       if (existingItem) {
-        state.items = state.items.filter((item) => item.id !== id);
-        toast.success("Item removed from wishlist.");
+        state.items = state.items.filter((item) => item.id !== id)
+        toast.success("Item removed from wishlist.")
       } else {
-        toast.info("Item not found in the wishlist.");
+        toast.info("Item not found in the wishlist.")
       }
     },
     clearWishlist: (state) => {
-      state.items = [];
-      toast.success("Wishlist cleared successfully.");
+      state.items = []
+      toast.success("Wishlist cleared successfully.")
     },
   },
-});
+})
 
 export const { addItemToWishlist, removeItemFromWishlist, clearWishlist } =
-  wishlistSlice.actions;
-export default wishlistSlice.reducer;
+  wishlistSlice.actions
+export default wishlistSlice.reducer
