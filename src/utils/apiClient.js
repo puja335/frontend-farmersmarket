3cb4612461a83f241683d67f495e7856be49f764
puja335 <shahipuja335@gmail.com>
2025-01-24 17:08:19 +0545

    modified apiClient.js

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,6 +1,6 @@
 // import { logout, refreshToken } from '@/features/auth/authSlice';
 // import store from '@/store';
-import axios from "axios";
+import axios from "axios"
 
 const apiClient = axios.create({
   baseURL:
@@ -8,52 +8,52 @@ const apiClient = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-});
+})
 
 // Request interceptor
 apiClient.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("BearerToken");
+    const token = localStorage.getItem("BearerToken")
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`
     }
-    return config;
+    return config
   },
   (error) => {
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
 // Response interceptor
 apiClient.interceptors.response.use(
   (response) => response,
   async (error) => {
-    const originalRequest = error.config;
+    const originalRequest = error.config
 
     // If the error status is 401 and we haven't already tried to refresh the token
     if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
+      originalRequest._retry = true
 
       try {
         // Attempt to refresh the token
-        const response = await apiClient.post("/auth/refresh-token");
-        const newToken = response.data.token;
+        const response = await apiClient.post("/auth/refresh-token")
+        const newToken = response.data.token
 
         // Store the new token
-        store.dispatch(refreshToken(newToken));
+        store.dispatch(refreshToken(newToken))
 
         // Retry the original request
-        originalRequest.headers.Authorization = `Bearer ${newToken}`;
-        return apiClient(originalRequest);
+        originalRequest.headers.Authorization = `Bearer ${newToken}`
+        return apiClient(originalRequest)
       } catch (refreshError) {
         // If refresh token fails, logout the user
-        store.dispatch(logout());
-        return Promise.reject(refreshError);
+        store.dispatch(logout())
+        return Promise.reject(refreshError)
       }
     }
 
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
-export default apiClient;
+export default apiClient
