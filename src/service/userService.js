9d0f15ff809a42c7dd0f9006207449d448982c32
puja335 <shahipuja335@gmail.com>
2025-01-24 17:07:10 +0545

    added userService.js

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,17 +1,17 @@
-import axios from "axios";
+import axios from "axios"
 
 export const registerUser = async (formData) => {
   const response = await axios.post(
     "http://localhost:5000/api/v1/auth/register",
     formData
-  );
-  return response.data;
-};
+  )
+  return response.data
+}
 
 export const loginUser = async (data) => {
   const response = await axios.post(
     "http://localhost:5000/api/v1/auth/login",
     data
-  );
-  return response.data;
-};
+  )
+  return response.data
+}
