07af69b5f651888d1608c2da7897f9c2de5675e8
puja335 <shahipuja335@gmail.com>
2025-01-24 17:07:44 +0545

    added rootReducer.js

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,12 +1,12 @@
-import { combineReducers } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit"
 
-import authSlice from "../features/auth/authSlice";
-import cartSlice from "../features/cart/cartSlice";
-import wishlistSlice from "../features/wishlist/wishlistSlice";
+import authSlice from "../features/auth/authSlice"
+import cartSlice from "../features/cart/cartSlice"
+import wishlistSlice from "../features/wishlist/wishlistSlice"
 const rootReducer = combineReducers({
   auth: authSlice,
   cart: cartSlice,
   wishlist: wishlistSlice,
-});
+})
 
-export default rootReducer;
+export default rootReducer
